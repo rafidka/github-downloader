9cf945967e0530523103fda87587c5c95ef93ede
Rafid Al-Humaimidi <rafidka@gmail.com>
2022-04-20 23:16:14 -0700

    Add Octokit library

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+import { Octokit } from "octokit";
+
+async function main() {
+  if (!process.env.GITHUB_TOKEN) {
+    throw Error("GITHUB_TOKEN is not set");
+  }
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
+
+  console.log(`Hello ${login}!`);
+}
+
+main().then();
