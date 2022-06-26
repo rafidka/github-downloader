e504b7e320772e09c0d05c6dff47873ee30a67de
Rafid Al-Humaimidi <rafidka@gmail.com>
2022-06-26 12:42:26 -0700

    Support getting more than 1k repos + Deduplication
    GitHub limits the number of results that can be retrieved using their
    search APIs to 1000. To work around that, I changed the code to make
    multiple requests to the backend, where each subsequent request
    limiting the results to star/fork count below the minimum value
    obtained from the previous request.
    
    I also added a dedulpication mechaism to the code. This is required
    because, for some reason, GitHub API (or perhaps Octokit) is
    returning deuplicate results.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { exec, ExecOptions } from "child_process";
 import { promisify } from "util";
 import { join as joinPath } from "path";
 import * as fs from "fs";
+import * as _ from "lodash";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import {
@@ -109,6 +110,7 @@ const OrderOptions = ["asc", "desc"] as const;
 type Order = typeof OrderOptions[number];
 
 const GITHUB_SEARCH_MAX_PAGE_SIZE = 100;
+const GITHUB_SEARCH_MAX_RESULT_COUNT = 1000;
 
 /**
  * Retrieves a list of repositories for a given language, sorted by the number
@@ -118,15 +120,27 @@ const GITHUB_SEARCH_MAX_PAGE_SIZE = 100;
  * @param maxRepoCount The maximum number of repos to return.
  * @param sort The field to sort the repos by.
  * @param order Whether the sort ascendingly or descendingly.
+ * @param sortFieldMaxValue The maximum value of the sorting field. This is
+ * useful to enable retrieving more than the maximum number of results GitHub
+ * allows. So, after the first request to this function, one can make another
+ * call, setting the value of this parameter to the minimum value retrieved
+ * from the last call.
  *
  * @return A list of repositories.
  */
-async function searchRepos(
+async function searchReposHelper(
   language: Language,
   maxRepoCount: number,
   sort: Sort = "forks",
-  order: Order = "desc"
+  order: Order = "desc",
+  sortFieldMaxValue?: number
 ): Promise<SearchRepoResultItem[]> {
+  if (maxRepoCount > GITHUB_SEARCH_MAX_RESULT_COUNT) {
+    throw Error(
+      `maxRepoCount cannot be greater than ${GITHUB_SEARCH_MAX_RESULT_COUNT}`
+    );
+  }
+
   const pageSize = Math.min(maxRepoCount, GITHUB_SEARCH_MAX_PAGE_SIZE);
   const pageCount = Math.ceil(maxRepoCount / pageSize);
 
@@ -137,20 +151,30 @@ async function searchRepos(
     );
   }
 
+  let query = `language:${language} archived:false`;
+  if (sortFieldMaxValue) {
+    query = `${sort}:<${sortFieldMaxValue} ${query}`;
+  }
+
   // Paginated requests to retrieve the desired number of repos.
-  const itemsFromAllPages = [];
+  const itemsFromAllPages: SearchRepoResultItem[] = [];
   for (let page = 1; page <= pageCount; page++) {
     logger.debug(`Searching for '${language}' repos [${page}/${pageCount}]...`);
-    const {
-      data: { items },
-    } = await octokit.rest.search.repos({
-      q: `language:${language} archived:false`,
+    const res = await octokit.rest.search.repos({
+      q: query,
       per_page: pageSize,
       page,
       sort,
       order,
     });
-    itemsFromAllPages.push(...items);
+    const {
+      data: { items },
+    } = res;
+
+    // Unfortunately, the results from the pages can overlap, so we deduplicate
+    // them.
+    const newItems = _.differenceBy(items, itemsFromAllPages, (x) => x.id);
+    itemsFromAllPages.push(...newItems);
   }
 
   logger.info(`Found ${itemsFromAllPages.length} ${language} repos:`);
@@ -159,6 +183,66 @@ async function searchRepos(
   });
 
   return itemsFromAllPages;
+}
+
+/**
+ * Similar to {@link searchReposHelper}, but is not limited by the maximum number of
+ * results GitHub allows.
+ *
+ * @param language The programming language of the repos to search.
+ * @param maxRepoCount The maximum number of repos to return.
+ * @param sort The field to sort the repos by.
+ * @param order Whether the sort ascendingly or descendingly.
+ *
+ * @return A list of repositories.
+ */
+export async function searchRepos(
+  language: Language,
+  maxRepoCount: number,
+  sort: Sort = "forks",
+  order: Order = "desc"
+) {
+  const maxRepoCountForOneCall = Math.min(
+    maxRepoCount,
+    GITHUB_SEARCH_MAX_RESULT_COUNT
+  );
+  const callCount = Math.ceil(maxRepoCount / maxRepoCountForOneCall);
+
+  let sortFieldMaxValue: number | undefined = undefined;
+  const itemsFromAllCalls: SearchRepoResultItem[] = [];
+  for (let call = 1; call <= callCount; call++) {
+    logger.info(
+      `Calling searchRepos to search for '${language}' repos [${call}/${callCount}].`
+    );
+    let repos = await searchReposHelper(
+      language,
+      maxRepoCountForOneCall,
+      sort,
+      order,
+      sortFieldMaxValue
+    );
+
+    if (repos.length == 0) {
+      // No more results.
+      break;
+    }
+
+    // Deduplicate the results. This is necessary to remove duplicates at the
+    // boundaries of the sort field max value.
+    const newItems = _.differenceBy(repos, itemsFromAllCalls, (x) => x.id);
+    itemsFromAllCalls.push(...newItems);
+
+    // Update the sort field max value to retrieve more results.
+    if (sort === "stars") {
+      sortFieldMaxValue = Math.min(
+        ...repos.map((repo) => repo.stargazers_count)
+      );
+    } else {
+      sortFieldMaxValue = Math.min(...repos.map((repo) => repo.forks_count));
+    }
+  }
+
+  return itemsFromAllCalls;
 }
 
 /**
